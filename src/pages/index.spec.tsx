1f0c9ae65f8ce2bb096d9946d950e9ac7ccb3835
Gisele Pecapedra <gbspecapedra@gmail.com>
2022-05-13 10:11:37 -0300

    feat: setup tests

---
+++ src/pages/index.spec.tsx
@@ -0,0 +1,11 @@
+import { render } from "@testing-library/react";
+import Home from ".";
+
+test("display welcome page", () => {
+  const { getByRole } = render(<Home />);
+  expect(
+    getByRole("heading", {
+      name: "Welcome to Next.js!",
+    })
+  ).toBeInTheDocument();
+});
---
+++ src/pages/index.tsx
@@ -0,0 +1,7 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+  return <h1>Welcome to Next.js!</h1>;
+};
+
+export default Home;
