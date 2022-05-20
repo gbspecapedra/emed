12e09ec109b7e6c350ea24749e85381044d2c86d
Gisele Pecapedra <gbspecapedra@gmail.com>
2022-05-20 17:48:46 -0300

    ref: wip dashboard

--- web/src/pages/dashboard/index.tsx
+++ web/src/pages/dashboard/index.tsx
@@ -21,9 +21,11 @@ import {
   MenuItem,
   MenuList,
 } from '@chakra-ui/react'
+import { GetServerSideProps } from 'next'
+import { parseCookies } from 'nookies'
 import React, { ReactNode } from 'react'
-import { ReactText } from 'react'
 import { IconType } from 'react-icons'
+import { AiOutlineUser } from 'react-icons/ai'
 import {
   FiHome,
   FiTrendingUp,
@@ -35,6 +37,7 @@ import {
   FiChevronDown,
 } from 'react-icons/fi'
 import { useAuth } from '../../services/contexts/AuthContext'
+import { EMED_TOKEN } from '../../utils'
 
 interface LinkItemProps {
   name: string
@@ -100,7 +103,7 @@ const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
     >
       <Flex h="20" alignItems="center" mx="8" justifyContent="space-between">
         <Text fontSize="2xl" fontFamily="monospace" fontWeight="bold">
-          Logo
+          eMED
         </Text>
         <CloseButton display={{ base: 'flex', md: 'none' }} onClick={onClose} />
       </Flex>
@@ -115,7 +118,7 @@ const SidebarContent = ({ onClose, ...rest }: SidebarProps) => {
 
 interface NavItemProps extends FlexProps {
   icon: IconType
-  children: ReactText
+  children: ReactNode
 }
 const NavItem = ({ icon, children, ...rest }: NavItemProps) => {
   return (
@@ -157,7 +160,7 @@ interface MobileProps extends FlexProps {
   onOpen: () => void
 }
 const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
-  const { signOut } = useAuth()
+  const { professional, signOut } = useAuth()
 
   return (
     <Flex
@@ -185,7 +188,7 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
         fontFamily="monospace"
         fontWeight="bold"
       >
-        Logo
+        eMED
       </Text>
 
       <HStack spacing={{ base: '0', md: '6' }}>
@@ -205,9 +208,8 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
               <HStack>
                 <Avatar
                   size={'sm'}
-                  src={
-                    'https://images.unsplash.com/photo-1619946794135-5bc917a27793?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9'
-                  }
+                  name={professional?.name}
+                  icon={<AiOutlineUser fontSize="1.5rem" />}
                 />
                 <VStack
                   display={{ base: 'none', md: 'flex' }}
@@ -215,9 +217,9 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
                   spacing="1px"
                   ml="2"
                 >
-                  <Text fontSize="sm">Justina Clark</Text>
+                  <Text fontSize="sm">{professional?.name}</Text>
                   <Text fontSize="xs" color="gray.600">
-                    Admin
+                    {professional?.type}
                   </Text>
                 </VStack>
                 <Box display={{ base: 'none', md: 'flex' }}>
@@ -242,4 +244,21 @@ const MobileNav = ({ onOpen, ...rest }: MobileProps) => {
       </HStack>
     </Flex>
   )
+}
+
+export const getServerSideProps: GetServerSideProps = async ctx => {
+  const { [EMED_TOKEN]: token } = parseCookies(ctx)
+
+  if (!token) {
+    return {
+      redirect: {
+        destination: '/',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: {},
+  }
 }
