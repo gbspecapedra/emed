e436ad4e8400085eb31854dd82aead3947e2acaa
Gisele Pecapedra <gbspecapedra@gmail.com>
2022-06-10 20:44:52 -0300

    ref: wip pdf file

--- web/src/pages/dashboard/attendance/[id]/prescriptions/index.tsx
+++ web/src/pages/dashboard/attendance/[id]/prescriptions/index.tsx
@@ -1,13 +1,20 @@
-import { Button, Flex, Heading, HStack } from '@chakra-ui/react'
-import ReactPDF from '@react-pdf/renderer'
+import {
+  Button,
+  Flex,
+  Heading,
+  HStack,
+  Stack,
+  useColorModeValue,
+} from '@chakra-ui/react'
+import { PDFDownloadLink } from '@react-pdf/renderer'
 import { GetServerSideProps } from 'next'
+import { useRouter } from 'next/router'
 import { parseCookies } from 'nookies'
 import { Column } from 'primereact/column'
 import React, { useState } from 'react'
-import { useForm } from 'react-hook-form'
+import { FormProvider, useForm } from 'react-hook-form'
 import { FaTimes } from 'react-icons/fa'
 import { GoPlus } from 'react-icons/go'
-import { FormLayout } from '@/components/form/layout/FormLayout'
 import { Select } from '@/components/form/select'
 import { PDF } from '@/components/pdf'
 import Table from '@/components/table'
@@ -20,8 +27,8 @@ import { useNotification } from '@/services/hooks/useNotification'
 import { EMED_TOKEN } from '@/utils/constants'
 
 interface IPrescriptionsInputs {
-  exam?: Pick<Exam, 'id'>
-  medicine?: Pick<Medicine, 'id'>
+  exam?: Exam
+  medicine?: Medicine
 }
 
 interface IPrescriptionsProps {
@@ -37,6 +44,7 @@ const Prescriptions: React.FC<IPrescriptionsProps> = ({
   exams,
   medicines,
 }) => {
+  const router = useRouter()
   const notification = useNotification()
 
   const [listOfExams, setListOfExams] = useState<Pick<Exam, 'id'>[]>([])
@@ -75,7 +83,6 @@ const Prescriptions: React.FC<IPrescriptionsProps> = ({
           medicineIds,
         })
         .then(() => {
-          ReactPDF.render(<PDF />, `${__dirname}/example.pdf`)
           notification.success({
             title: 'Prescription successfully generated',
             to: `/dashboard/attendance/${attendanceId}`,
@@ -90,109 +97,154 @@ const Prescriptions: React.FC<IPrescriptionsProps> = ({
   }
 
   return (
-    <FormLayout
-      methods={methods}
-      header="Prescriptions"
-      onSubmit={handleUpdateAttendance}
-      returnTo={`/dashboard/attendance/${attendanceId}`}
-    >
-      <Heading lineHeight={1.1} fontSize={{ base: '2xl', sm: '3xl' }}>
-        Exams
-      </Heading>
-      <Table
-        values={listOfExams}
-        header={
-          <HStack>
-            <Select
-              name="exam"
-              options={exams.map(({ id, name }: Exam) => {
-                return {
-                  label: name,
-                  value: `${id}`,
-                }
-              })}
-            />
-            <Button
-              colorScheme="green"
-              variant="outline"
-              onClick={() => addExam()}
-            >
-              <GoPlus />
-            </Button>
-          </HStack>
-        }
+    <FormProvider {...methods}>
+      <Flex
+        as="form"
+        align={'center'}
+        justify={'center'}
+        onSubmit={methods.handleSubmit(handleUpdateAttendance)}
       >
-        <Column field="name" />
-        <Column
-          body={row => (
-            <Flex flexDirection="row" justifyContent="end">
-              <Button
-                colorScheme="red"
-                variant="ghost"
-                onClick={() => {
-                  setListOfExams(listOfExams.filter(({ id }) => id !== row.id))
-                }}
-              >
-                <FaTimes />
-              </Button>
-            </Flex>
-          )}
-          exportable={false}
-        />
-      </Table>
-      <Heading lineHeight={1.1} fontSize={{ base: '2xl', sm: '3xl' }}>
-        Medicines
-      </Heading>
-      <Table
-        values={listOfMedicines}
-        header={
-          <HStack>
-            <Select
-              name="medicine"
-              options={medicines.map(
-                ({ id, name, concentration, usage, producer }: Medicine) => {
-                  return {
-                    label: `${name} ${concentration} ${usage} (${producer})`,
-                    value: `${id}`,
-                  }
-                },
+        <Stack
+          spacing={4}
+          w={'full'}
+          maxW={'5xl'}
+          bg={useColorModeValue('white', 'gray.700')}
+          rounded={'xl'}
+          boxShadow={'lg'}
+          p={6}
+          my={6}
+        >
+          <Heading lineHeight={1.1} fontSize={{ base: '2xl', sm: '3xl' }}>
+            Prescriptions
+          </Heading>
+
+          <Heading size={'md'}>Exams</Heading>
+          <Table
+            values={listOfExams}
+            header={
+              <HStack>
+                <Select
+                  name="exam"
+                  options={exams.map(({ id, name }: Exam) => {
+                    return {
+                      label: name,
+                      value: `${id}`,
+                    }
+                  })}
+                />
+                <Button
+                  colorScheme="green"
+                  variant="outline"
+                  onClick={() => addExam()}
+                >
+                  <GoPlus />
+                </Button>
+              </HStack>
+            }
+          >
+            <Column field="name" />
+            <Column
+              body={row => (
+                <Flex flexDirection="row" justifyContent="end">
+                  <Button
+                    colorScheme="red"
+                    variant="ghost"
+                    onClick={() => {
+                      setListOfExams(
+                        listOfExams.filter(({ id }) => id !== row.id),
+                      )
+                    }}
+                  >
+                    <FaTimes />
+                  </Button>
+                </Flex>
               )}
+              exportable={false}
             />
+          </Table>
+
+          <Heading size={'md'}>Medicines</Heading>
+          <Table
+            values={listOfMedicines}
+            header={
+              <HStack>
+                <Select
+                  name="medicine"
+                  options={medicines.map(
+                    ({
+                      id,
+                      name,
+                      concentration,
+                      usage,
+                      producer,
+                    }: Medicine) => {
+                      return {
+                        label: `${name} ${concentration} ${usage} (${producer})`,
+                        value: `${id}`,
+                      }
+                    },
+                  )}
+                />
+                <Button
+                  colorScheme="green"
+                  variant="outline"
+                  onClick={() => addMedicine()}
+                >
+                  <GoPlus />
+                </Button>
+              </HStack>
+            }
+          >
+            <Column
+              body={row =>
+                `${row.name} ${row.concentration} ${row.usage} (${row.producer})`
+              }
+            />
+            <Column
+              body={row => (
+                <Flex flexDirection="row" justifyContent="end">
+                  <Button
+                    colorScheme="red"
+                    variant="ghost"
+                    onClick={() => {
+                      setListOfMedicines(
+                        listOfMedicines.filter(({ id }) => id !== row.id),
+                      )
+                    }}
+                  >
+                    <FaTimes />
+                  </Button>
+                </Flex>
+              )}
+              exportable={false}
+            />
+          </Table>
+
+          <Stack spacing={6} direction={['column', 'row']}>
             <Button
-              colorScheme="green"
-              variant="outline"
-              onClick={() => addMedicine()}
+              w="full"
+              colorScheme="red"
+              onClick={() =>
+                router.push(`/dashboard/attendance/${attendanceId}`)
+              }
             >
-              <GoPlus />
+              Cancel
             </Button>
-          </HStack>
-        }
-      >
-        <Column
-          body={row =>
-            `${row.name} ${row.concentration} ${row.usage} (${row.producer})`
-          }
-        />
-        <Column
-          body={row => (
-            <Flex flexDirection="row" justifyContent="end">
-              <Button
-                colorScheme="red"
-                variant="ghost"
-                onClick={() => {
-                  setListOfMedicines(
-                    listOfMedicines.filter(({ id }) => id !== row.id),
-                  )
-                }}
-              >
-                <FaTimes />
-              </Button>
-            </Flex>
-          )}
-          exportable={false}
-        />
-      </Table>
-    </FormLayout>
+            <Button
+              w="full"
+              colorScheme="green"
+              isLoading={methods.formState.isSubmitting}
+              disabled={!methods.formState.isValid}
+              data-testid="submit-form-button"
+            >
+              <PDFDownloadLink document={<PDF />} fileName="somename.pdf">
+                Generate
+              </PDFDownloadLink>
+            </Button>
+          </Stack>
+        </Stack>
+      </Flex>
+    </FormProvider>
   )
 }
 
